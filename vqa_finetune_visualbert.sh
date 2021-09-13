030d1081ca79f304647072e7031442c828713508
chenkangyang <chenkangyang98@gmail.com>
2021-09-13 15:56:23 +0800

    生成测试集结果文件的脚本

--- vqa_finetune_visualbert.sh
+++ vqa_finetune_visualbert.sh
@@ -3,4 +3,4 @@ export PYTHONPATH=$PYTHONPATH:/home/chenkangyang/workspace/visualbert/
 
 cd /home/chenkangyang/workspace/visualbert
 
-CUDA_VISIBLE_DEVICES=2 python visualbert/models/train.py -folder logs/vqa_finetune -config /home/chenkangyang/workspace/visualbert/visualbert/configs/vqa/fine-tune.json
+CUDA_VISIBLE_DEVICES=3 python visualbert/models/train.py -folder logs/vqa_finetune_0831 -config /home/chenkangyang/workspace/visualbert/visualbert/configs/vqa/fine-tune.json
