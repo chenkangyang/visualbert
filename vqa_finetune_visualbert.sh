b502793d288bfb98149903b2b62f28931328a4ce
chenkangyang <chenkangyang98@gmail.com>
2021-09-28 11:05:54 +0800

    Update Vqa fintune log

--- vqa_finetune_visualbert.sh
+++ vqa_finetune_visualbert.sh
@@ -1,6 +1,6 @@
-export PYTHONPATH=$PYTHONPATH:/home/chenkangyang/workspace/visualbert/visualbert/
-export PYTHONPATH=$PYTHONPATH:/home/chenkangyang/workspace/visualbert/
+export PYTHONPATH=$PYTHONPATH:$HOME/workspace/visualbert/visualbert/
+export PYTHONPATH=$PYTHONPATH:$HOME/workspace/visualbert/
 
-cd /home/chenkangyang/workspace/visualbert
+cd $HOME/workspace/visualbert
 
-CUDA_VISIBLE_DEVICES=3 python visualbert/models/train.py -folder logs/vqa_finetune_0831 -config /home/chenkangyang/workspace/visualbert/visualbert/configs/vqa/fine-tune.json
+CUDA_VISIBLE_DEVICES=0 python visualbert/models/train.py -folder logs/vqa_finetune_0926 -config $HOME/workspace/visualbert/vqa-fine-tune.json
